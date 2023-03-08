f284ad91bfcee6e270fbc6ac24ef978e5a73b350
latuan1 <125745639+latuan1@users.noreply.github.com>
2023-03-08 09:31:03 +0700

    Add files via upload

---
+++ BT06/6C.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n,so[10],check[10];
+
+void printkq()
+{
+    for (int i=0;i<n;i++)cout << so[i];
+    cout << endl;
+}
+
+void test(int i)
+{
+    for (int j=1;j<=n;j++)
+    {
+        if (check[j]==1)
+        {
+            so[i]=j;
+            check[j]=0;
+            if (i==n-1)printkq();
+            else test(i+1);
+            check[j]=1;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n;
+    for (int i=0;i<10;i++)check[i]=1;
+    test(0);
+    return 0;
+}
